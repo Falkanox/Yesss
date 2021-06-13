bcbe0831491ad1c42a0732e7b0cb84975ebbc22f
Falkanox <46320591+Falkanox@users.noreply.github.com>
2021-06-14 00:19:16 +0200

    Update InventoryClick.java

--- events/InventoryClick.java
+++ events/InventoryClick.java
@@ -1,7 +1,11 @@
 package fr.falkanox.bingo.events;
 
+import dev.jcsoftware.jscoreboards.JPerPlayerMethodBasedScoreboard;
 import fr.falkanox.bingo.Bingo;
 import fr.falkanox.bingo.states.GState;
+import fr.falkanox.bingo.teams.BasicScoreboard;
+import fr.falkanox.bingo.teams.BlueTeam;
+import fr.falkanox.bingo.teams.RedTeam;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,13 +15,23 @@ import java.util.UUID;
 
 public class InventoryClick implements Listener {
 
+    RedTeam redTeamClass;
+    BlueTeam blueTeamClass;
+    BasicScoreboard basicScoreboard;
+    JPerPlayerMethodBasedScoreboard scoreboard;
+
     private Bingo main;
-    public InventoryClick(Bingo main){
+    public InventoryClick(JPerPlayerMethodBasedScoreboard scoreboard,Bingo main){
         this.main = main;
+        this.scoreboard = scoreboard;
     }
 
     @EventHandler
     public void onClick(InventoryClickEvent e){
+
+        redTeamClass = new RedTeam(scoreboard, main);
+        blueTeamClass = new BlueTeam(scoreboard, main);
+        basicScoreboard = new BasicScoreboard(scoreboard, main);
 
         Player p = (Player) e.getWhoClicked();
 
@@ -36,13 +50,13 @@ public class InventoryClick implements Listener {
 
             if(e.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase("§cÉquipe Rouge")){
 
-                main.redTeam.addToRedTeam(p.getUniqueId(), p);
+                blueTeamClass.addToBlueTeam(p);
 
             }
 
             if(e.getCurrentItem().getItemMeta().getDisplayName().equalsIgnoreCase("§bÉquipe Bleue")){
 
-                main.blueTeam.addToBlueTeam(p.getUniqueId(), p);
+                redTeamClass.addToRedTeam(p);
 
             }
 
