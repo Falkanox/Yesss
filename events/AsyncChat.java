0a415fab9ba549bff05bba6510310e5de2e99358
Falkanox <46320591+Falkanox@users.noreply.github.com>
2021-06-14 00:18:38 +0200

    Update AsyncChat.java

--- events/AsyncChat.java
+++ events/AsyncChat.java
@@ -1,7 +1,11 @@
 package fr.falkanox.bingo.events;
 
+import dev.jcsoftware.jscoreboards.JPerPlayerMethodBasedScoreboard;
 import fr.falkanox.bingo.Bingo;
 import fr.falkanox.bingo.states.GState;
+import fr.falkanox.bingo.teams.BasicScoreboard;
+import fr.falkanox.bingo.teams.BlueTeam;
+import fr.falkanox.bingo.teams.RedTeam;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,22 +13,34 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class AsyncChat implements Listener {
 
+    RedTeam redTeamClass;
+    BlueTeam blueTeamClass;
+    BasicScoreboard basicScoreboard;
+    JPerPlayerMethodBasedScoreboard scoreboard;
+
     private Bingo main;
-    public AsyncChat(Bingo main){ this.main = main;}
+    public AsyncChat(JPerPlayerMethodBasedScoreboard scoreboard, Bingo main){
+        this.main = main;
+        this.scoreboard = scoreboard;
+    }
 
     @EventHandler
     public void onChat(AsyncPlayerChatEvent e){
+
+        redTeamClass = new RedTeam(scoreboard, main);
+        blueTeamClass = new BlueTeam(scoreboard, main);
+        basicScoreboard = new BasicScoreboard(scoreboard, main);
 
         Player p = e.getPlayer();
 
         if(main.isState(GState.WAITING)){
 
-            if(main.jblueTeam.isOnTeam(p.getUniqueId())){
+            if(scoreboard.findTeam("Bleue").get().isOnTeam(p.getUniqueId())){
 
                 e.setFormat("§bBleue " + "%1$s" + " §f: §7" + "%2$s");
 
 
-            } else if(main.jredTeam.isOnTeam(p.getUniqueId())){
+            } else if(scoreboard.findTeam("Rouge").get().isOnTeam(p.getUniqueId())){
 
                 e.setFormat("§cRouge " + "%1$s" + " §f: §7" + "%2$s");
 
